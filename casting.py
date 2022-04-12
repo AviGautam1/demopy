0c92d912951542e2507509347ba380eebfea9719
user <avifetian@gmail.com>
2022-04-12 17:10:12 +0530

    python casting

---
+++ casting.py
@@ -0,0 +1,28 @@
+# by using int()
+
+x = int(8)
+y = int(5.8)
+z = int("55")
+print(x)
+print(y)
+print(z)
+
+# by using float()
+
+x = float(25)
+y = float(2.8)
+z = float("36")
+w = float("25.8")
+print(x)
+print(y)
+print(z)
+print(w)
+
+# by using str()
+
+x = str("f8")
+y = str(58)
+z = str(26.7)
+print(x)
+print(y)
+print(z)
