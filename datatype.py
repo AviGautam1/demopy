904351c441a8dc4f1c80f4ab8bc10d686007e4a9
user <avifetian@gmail.com>
2022-04-12 13:30:42 +0530

    python data type

--- datatype.py
+++ datatype.py
@@ -2,3 +2,68 @@
 x = "hey"
 print(x)
 print(type(x))
+
+# python int data type
+x = 5
+print(x)
+print(type(x))
+
+# python float data type
+x = 5.2
+print(x)
+print(type(x))
+
+# python complex data type
+x = 1j
+print(x)
+print(type(x))
+
+# python list data type
+x = ["apple", "mango"]
+print(x)
+print(type(x))
+
+# python tuple data type
+x = ("harish", "anil")
+print(x)
+print(type(x))
+
+# python range data type
+x = 8
+print(x)
+print(type(x))
+
+# python dict data type
+x = {"name" : "ramu", "age" : 30}
+print(x)
+print(type(x))
+
+# python set data type
+x = {"avi", "deepak"}
+print(x)
+print(type(x))
+
+# python frozenset data type
+x = ({"bababa", "apple"})
+print(x)
+print(type(x))
+
+# python bool data type
+x = True
+print(x)
+print(type(x))
+
+# python byte data type
+x = b"Hello"
+print(x)
+print(type(x))
+
+# python bytearray data type
+x = bytearray(5)
+print(x)
+print(type(x))
+
+# python memoryview data type
+x = memoryview(bytes(8))
+print(x)
+print(type(x))
