8d64f1eb0038c9f63fb0e0f93298d2f12d7a6804
user <avifetian@gmail.com>
2022-04-12 12:51:45 +0530

    python variables

--- demo.py
+++ demo.py
@@ -1,2 +1,44 @@
-print("Hello")
-print("hey")
+x = 5
+y = "fruit"
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+
+# Assigning multiple values
+x, y, z = "honda", "jupiter", "suzuki"
+print(x)
+print(y)
+print(z)
+
+x = "honda", "jupiter", "suzuki"
+print(x)
+
+# unpacking values
+list = ["lion", "dog", "cat"]
+x, y, z = list
+print(x)
+print(y)
+print(z)
+
+# output variables
+
+x = 10
+y = "sanjeev"
+print(x,y)
+
+# global variables
+
+x = "deepak"
+def myfunc():
+    x = "ankur"
+    print(x)
+myfunc()
+print(x)
+
+def myfunc():
+    global x
+    x = "udit"
+myfunc()
+print(x)
+
