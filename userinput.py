7c252864fc8f39a3394738559d77b849256c222d
avinesh <avifetian@gmail.com>
2022-04-20 21:30:36 +0530

    python user input

---
+++ userinput.py
@@ -0,0 +1,7 @@
+"""Python 3.6 uses the input() method.
+Python 2.7 uses the raw_input() method."""
+password = input("Enter Password:")
+print("Password is: " + password)
+
+username = raw_input("Enter username:")
+print("Username is: " + username)
